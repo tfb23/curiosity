e1a05548f29e5c9f7de59e5a2b03f14585339b24
Toushiful Ferdous Badhan <68116912+Toushiful-Ferdous-Badhan@users.noreply.github.com>
2020-07-24 21:02:41 +0600

    Create 263A. Beautiful Matrix.cpp

---
+++ 263A. Beautiful Matrix.cpp
@@ -0,0 +1,21 @@
+//Problem link:  https://codeforces.com/problemset/problem/263/A
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int x=0;
+	for (int i=1; i<=5 ;i++)
+	{
+		for(int j=1; j<=5; j++)
+		{
+			cin >> x;
+			if (x==1) 
+			{
+				cout <<abs(3-i)+abs(3-j)<<endl;
+			}
+		}
+	}
+	return 0;
+}
