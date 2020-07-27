d7d59380cdbcd654cba339814285d2aee7d7528a
Toushiful Ferdous Badhan <68116912+Toushiful-Ferdous-Badhan@users.noreply.github.com>
2020-07-27 18:31:54 +0600

    Update 688B. Lovely Palindromes.cpp

--- 688B. Lovely Palindromes.cpp
+++ 688B. Lovely Palindromes.cpp
@@ -12,3 +12,19 @@ int main()
 	reverse(s.begin(), s.end());
 	cout<<s<<endl;
 }
+
+/*
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	string s;
+	cin>>s;
+	cout<<s;
+	for (int i=s.size()-1; i>=0; i--) 
+	cout<<s[i];
+	puts("");
+}
+
+*/
