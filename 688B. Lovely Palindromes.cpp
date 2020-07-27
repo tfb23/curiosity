4752feebbc66a4fb145c0c5a6c41cfda2dc46d23
Toushiful Ferdous Badhan <68116912+Toushiful-Ferdous-Badhan@users.noreply.github.com>
2020-07-27 18:26:52 +0600

    Create 688B. Lovely Palindromes.cpp

---
+++ 688B. Lovely Palindromes.cpp
@@ -0,0 +1,14 @@
+//Problem link: https://codeforces.com/problemset/problem/688/B
+
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+	string s;
+	cin>>s;
+	cout<<s;
+	reverse(s.begin(), s.end());
+	cout<<s<<endl;
+}
