3a0d000e108bc0a78a509cbcbbb7c5b487d9687f
Toushiful Ferdous Badhan <68116912+tfbadhan@users.noreply.github.com>
2020-07-30 18:21:23 +0600

    Create 282A. Bit++.cpp

---
+++ 282A. Bit++.cpp
@@ -0,0 +1,20 @@
+//  https://codeforces.com/contest/282/problem/A
+
+
+#include<bits/stdc++.h>
+#include<string.h>
+using namespace std;
+
+int main()
+{
+     int n , x=0;
+     string s;
+     cin>>n;
+     while(n--){
+        cin >>s;
+        if(s[1]=='+')x++;
+        else x--;
+     }
+     cout<<x<<endl;
+     return 0;
+}
