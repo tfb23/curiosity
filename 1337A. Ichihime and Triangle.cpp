9b042e420e7ed4691c18f3f8af8f95caaaa65477
Toushiful Ferdous Badhan <68116912+tfbadhan@users.noreply.github.com>
2020-07-30 18:36:27 +0600

    Create 1337A. Ichihime and Triangle.cpp

---
+++ 1337A. Ichihime and Triangle.cpp
@@ -0,0 +1,18 @@
+//  https://codeforces.com/contest/1337/problem/A
+
+
+#include<iostream>
+using namespace std;
+int main()
+
+{
+    int t;
+    cin>>t;
+    while(t--){
+            int a,b,c,d;
+            cin>>a>>b>>c>>d;
+
+            cout<<b <<" " << c<< " "<< c<<endl;
+    }
+    return 0;
+}
