0056211d2dbfbca2286fd81e6f8e711c9f39041b
Toushiful Ferdous Badhan <68116912+tfbadhan@users.noreply.github.com>
2020-07-30 18:26:50 +0600

    Create 1328A. Divisibility Problem.cpp

---
+++ 1328A. Divisibility Problem.cpp
@@ -0,0 +1,19 @@
+//  https://codeforces.com/contest/1328/problem/A
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+     int t;
+     cin>>t;
+     while(t--){
+        int a, b;
+        cin>>a>>b;
+        if (a%b==0)
+            cout<<0<<endl;
+        else
+            cout<<b-a%b<<endl;
+     }
+     return 0;
+}
