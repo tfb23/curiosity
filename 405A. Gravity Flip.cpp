3c1558e6faadfac3776fd3dedd86035628d4557d
Toushiful Ferdous Badhan <68116912+tfbadhan@users.noreply.github.com>
2020-08-30 20:41:01 +0600

    Create 405A. Gravity Flip.cpp

---
+++ 405A. Gravity Flip.cpp
@@ -0,0 +1,20 @@
+//    https://codeforces.com/problemset/problem/405/A
+
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, a[105];
+	cin>>n;
+	for (int i=0; i<n; i++)
+	{
+		cin>>a[i];
+	}
+	sort(a, a+n);
+	for (int i=0; i<n; i++)
+	{
+		cout<<a[i]<<" ";
+	}
+	return 0;
+}
