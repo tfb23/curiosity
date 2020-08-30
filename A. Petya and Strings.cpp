84f0f15c868fbc4563ac119606ceb9eb6ae6c54d
Toushiful Ferdous Badhan <68116912+tfbadhan@users.noreply.github.com>
2020-08-30 19:06:28 +0600

    Create A. Petya and Strings.cpp

---
+++ A. Petya and Strings.cpp
@@ -0,0 +1,39 @@
+//    https://codeforces.com/problemset/problem/112/A
+
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char str1[105],str2[105];
+    cin>>str1>>str2;
+    printf("%d\n",strcasecmp(str1,str2));  
+}
+
+
+    /*
+    for (int i = 0; i < strlen(s1); i++) {
+			s1[i] = toupper(s1[i]);
+		}
+
+		for (int i = 0; i < strlen(s2); i++) {
+			s2[i] = toupper(s2[i]);
+		}
+    */
+    
+    /*
+    for(i=0;a[i]!='\0';i++)
+	{
+		if(a[i]<='Z'&&a[i]>='A') a[i]+=32;
+		if(b[i]<='Z'&&b[i]>='A') b[i]+=32;
+		if(a[i]>b[i])
+		{
+			printf("1\n");
+			break;
+		}
+		else if(a[i]<b[i])
+		{
+			printf("-1\n");
+			break;
+		}
+    */
