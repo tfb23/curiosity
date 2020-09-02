16ea79f66c111ef7610d7d18791c3e1a5e3a31cc
Toushiful Ferdous Badhan <68116912+tfbadhan@users.noreply.github.com>
2020-09-02 18:25:14 +0600

    Create 572A. Arrays.cpp

---
+++ 572A. Arrays.cpp
@@ -0,0 +1,35 @@
+//    https://codeforces.com/problemset/problem/572/A
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int N=1e5;
+int main()
+{
+    int na,nb;
+    int k,m;
+    int a[N+5],b[N+5];
+    cin>>na>>nb;
+    cin>>k>>m;
+    for(int i=0;i<na;i++)
+       {
+       cin>>a[i];
+       }
+    for(int i=0;i<nb;i++)
+        {
+        cin>>b[i];
+        }
+    sort(a,a+na);
+    sort(b,b+nb);
+    reverse(b,b+nb);
+    if(a[k-1]<b[m-1])
+        {
+        cout<<"YES"<<endl;
+        }
+    else
+        {
+        cout<<"NO"<<endl;
+        }
+    return 0;
