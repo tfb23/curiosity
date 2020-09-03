aef4b35a46f570ec7d9355c1f190cb2550c5fc5e
Toushiful Ferdous Badhan <68116912+tfbadhan@users.noreply.github.com>
2020-09-03 19:48:29 +0600

    Create 124A. The number of positions.cpp

---
+++ 124A. The number of positions.cpp
@@ -0,0 +1,12 @@
+//    https://codeforces.com/problemset/problem/124/A
+
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int n, a, b;
+    cin>>n>>a>>b;
+    cout<<min(n-a, b+1)<<endl;
+}
