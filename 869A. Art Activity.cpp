312178c4bbbc8dd4817f050792cb22c59d145369
Toushiful Ferdous Badhan <68116912+tfbadhan@users.noreply.github.com>
2020-10-19 17:23:39 +0600

    Create 869A. Art Activity.cpp

---
+++ 869A. Art Activity.cpp
@@ -0,0 +1,28 @@
+//    https://codeforces.com/problemset/problem/869/A
+
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cstring>
+#include <map>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <bitset>
+#define  SIS std::ios::sync_with_stdio(false),cin.tie(0),cout.tie(0)
+
+using namespace std;
+const int inf = 0x3f3f3f3f;
+
+int main()
+{
+	SIS;
+	
+	puts("Karen");
+}
