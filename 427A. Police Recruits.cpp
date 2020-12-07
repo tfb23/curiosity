ea959c4afdd4653f546d7b1ffc803183cad72b59
Toushiful Ferdous Badhan <68116912+tfbadhan@users.noreply.github.com>
2020-12-07 17:22:15 +0600

    Create 427A. Police Recruits.cpp

---
+++ 427A. Police Recruits.cpp
@@ -0,0 +1,53 @@
+//    https://codeforces.com/problemset/problem/427/A
+
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <string>
+#include <map>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <bitset>
+#include <regex>
+#define   pi 3.14
+#define  SIS std::ios::sync_with_stdio(false),cin.tie(0),cout.tie(0)
+
+using namespace std;
+const int inf = 0x3f3f3f3f;
+const int N=1e5;
+
+int main()
+{
+	SIS;
+	
+	int i, n, cnt=0, sum=0, a[N];
+	cin>>n;
+	for(i=0; i<n; i++)
+	{
+		cin>>a[i];
+	}
+	
+	for(i=0; i<n; i++)
+	{
+		if(a[i]>0)
+		{
+			sum+=a[i];
+		}
+		else if(sum>0)
+		{
+			sum--;
+		}
+		else 
+		{
+			cnt++;
+		}
+	}
+	cout<<cnt<<endl;
+}
