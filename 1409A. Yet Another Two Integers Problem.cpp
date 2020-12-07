2246f38dbe86760fa00b1e01517222881ee6377a
Toushiful Ferdous Badhan <68116912+tfbadhan@users.noreply.github.com>
2020-12-07 15:33:44 +0600

    Create 1409A. Yet Another Two Integers Problem.cpp

---
+++ 1409A. Yet Another Two Integers Problem.cpp
@@ -0,0 +1,23 @@
+//    https://codeforces.com/problemset/problem/1409/A
+
+
+
+
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		long long a, b;
+		cin>>a>>b;
+		cout<<(abs(a-b)+9)/10<<endl;
+	}
+}
+
+
