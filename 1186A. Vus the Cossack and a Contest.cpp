902da5297bd355bb5d29d9badd774196ec696a15
Toushiful Ferdous Badhan <68116912+tfbadhan@users.noreply.github.com>
2020-12-08 18:38:27 +0600

    Create 1186A. Vus the Cossack and a Contest.cpp

---
+++ 1186A. Vus the Cossack and a Contest.cpp
@@ -0,0 +1,21 @@
+//    https://codeforces.com/problemset/problem/1186/A
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int i, n, m, k;
+	
+	cin>>n>>m>>k;
+	
+	if((n<=m) && (n<=k))
+	{
+		puts("YES\n");
+	}
+	else
+	{
+		puts("NO\n");
+	}
+}
