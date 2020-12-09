957e5ae928680313d21e71e5d9a3bfd5b597bc30
Toushiful Ferdous Badhan <68116912+tfbadhan@users.noreply.github.com>
2020-12-09 18:21:23 +0600

    Create 1454A. Special Permutation.cpp

---
+++ 1454A. Special Permutation.cpp
@@ -0,0 +1,19 @@
+//    https://codeforces.com/problemset/problem/1454/A
+
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int i, n, t;
+	cin>>t;
+	while(t--)
+	{
+		cin>>n;
+		for(i=2; i<=n; i++)
+		{
+			cout<<i<<" ";
+		}
+		cout<<1<<endl;
+	}
+}
