93b36af1c9cf995970ae7b54adfd274bae1a4041
Toushiful Ferdous Badhan <68116912+tfbadhan@users.noreply.github.com>
2020-12-13 15:30:33 +0600

    Create 1455A. Strange Functions.cpp

---
+++ 1455A. Strange Functions.cpp
@@ -0,0 +1,18 @@
+//    https://codeforces.com/problemset/problem/1455/A
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin>>t;
+	char s[123456];
+	
+	while(t--)
+	{
+		cin>>s;
+		cout<<strlen(s)<<endl;
+	}
+}
