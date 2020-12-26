d9c3c237e6edef2cecad41459eca39008b8daeca
Toushiful Ferdous Badhan <68116912+tfbadhan@users.noreply.github.com>
2020-12-26 22:35:40 +0600

    Create 1093A. Dice Rolling.cpp

---
+++ 1093A. Dice Rolling.cpp
@@ -0,0 +1,15 @@
+//    https://codeforces.com/problemset/problem/1093/A
+
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t, x;
+	cin>>t;
+	while(t--)
+	{
+		cin>>x;
+		cout<<x/2<<endl;
+	}
+}
