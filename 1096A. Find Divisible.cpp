9357c99ed3195ff1ba4584c2e6d791caf949e7c8
Toushiful Ferdous Badhan <68116912+tfbadhan@users.noreply.github.com>
2020-12-26 23:12:52 +0600

    Create 1096A. Find Divisible.cpp

---
+++ 1096A. Find Divisible.cpp
@@ -0,0 +1,15 @@
+//    https://codeforces.com/problemset/problem/1096/A
+
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t, l, r;
+	cin>>t;
+	while(t--)
+	{
+		cin>>l>>r;
+		cout<<l<<" "<<2*l<<endl;
+	}
+}
