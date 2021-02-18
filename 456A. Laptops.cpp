7534568ceb0baea6fabbe0144d09d17c03de2257
Toushiful Ferdous Badhan <68116912+tfbadhan@users.noreply.github.com>
2021-02-18 22:21:41 +0600

    Create 456A. Laptops.cpp

---
+++ 456A. Laptops.cpp
@@ -0,0 +1,29 @@
+//    https://codeforces.com/problemset/problem/456/A
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int i, n;
+	cin>>n;
+	pair<int, int> a[n];
+	
+	for(i=0; i<n; i++)
+	{
+		cin>>a[i].first>>a[i].second;
+	}
+	
+	sort(a, a+n);
+	
+	for(i=0; i<n-1; i++)
+	{
+		if(a[i].first<a[i+1].first && a[i].second>a[i+1].second)
+		{
+			cout<<"Happy Alex"<<endl;
+			return 0;
+		}
+	}
+	cout<<"Poor Alex"<<endl;
+}
