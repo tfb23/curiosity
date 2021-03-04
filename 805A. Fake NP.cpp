5cd932537eb21946128c7de53f12f6c96ab5b432
Toushiful Ferdous Badhan <68116912+tfbadhan@users.noreply.github.com>
2021-03-04 15:53:10 +0600

    Create 805A. Fake NP.cpp

---
+++ 805A. Fake NP.cpp
@@ -0,0 +1,14 @@
+//    https://codeforces.com/problemset/problem/805/A
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin>>n>>m;
+    
+    if(n==m) cout<<n<<endl;
+    else cout<<2<<endl;
+}
