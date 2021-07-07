1b42e3316fcb62c22d38406790733ae6ce555903
Toushiful Ferdous Badhan <68116912+tfbadhan@users.noreply.github.com>
2021-07-07 22:34:53 +0600

    Create 1447A. Add Candies.cpp

---
+++ 1447A. Add Candies.cpp
@@ -0,0 +1,24 @@
+//    https://codeforces.com/problemset/problem/1447/A
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int t, n, m;
+	cin>>t;
+	while(t--)
+	{
+		cin>>n;
+		{
+			cout<<n<<endl;
+			
+			for(int i=1; i<=n; i++)
+			{
+				cout<<i<<" ";
+			}
+			cout<<endl;
+		}
+	}
+}
