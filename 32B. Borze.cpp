28431874c2c5b3b4447d70e9bc067abcf1ee68df
Toushiful Ferdous Badhan <68116912+tfbadhan@users.noreply.github.com>
2021-09-01 08:55:13 +0600

    Create 32B. Borze.cpp

---
+++ 32B. Borze.cpp
@@ -0,0 +1,27 @@
+//    https://codeforces.com/problemset/problem/32/B
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int i, l;
+	char s[123456];
+	cin >> s;
+	l = strlen(s);
+	
+	for(i = 0; i < l; i++) {
+		if(s[i] == '.') {
+			cout << 0 ;
+		}
+		if((s[i] == '-') && (s[i+1] == '.')) {
+			cout << 1 ;
+			i++;
+		}
+		if((s[i] == '-') && (s[i+1] == '-')) {
+			cout << 2 ;
+			i++;
+		}
+	}
+	cout << endl;
+}
