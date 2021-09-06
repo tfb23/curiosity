a20ccc4383ba0f31853f6040626cd3bc0a93455e
Toushiful Ferdous Badhan <68116912+tfbadhan@users.noreply.github.com>
2021-09-06 19:17:16 +0600

    Create 43A. Football.cpp

---
+++ 43A. Football.cpp
@@ -0,0 +1,25 @@
+//    https://codeforces.com/problemset/problem/43/A
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n, goal = 0;
+	cin >> n;
+	string team, win;
+	while(n--) {
+		if(goal != 0) {
+			cin >> team;
+			if(team == win) goal += 1;
+			else goal -= 1;
+		}
+		
+		else {
+			cin >> win;
+			goal = 1;
+		}
+	}
+	cout << win << endl;
+	return 0;
+}
