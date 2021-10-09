cb0ca30a4ba25507d89001075db44c2246d11040
Toushiful Ferdous Badhan <68116912+tfbadhan@users.noreply.github.com>
2021-10-09 16:40:03 +0600

    Create 1553A. Digits Sum.cpp

---
+++ 1553A. Digits Sum.cpp
@@ -0,0 +1,14 @@
+//    https://codeforces.com/problemset/problem/1553/A
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n;
+	cin >> t;
+	while(t--) {
+		cin >> n;
+		cout << (n + 1) / 10 << endl;
+	}
+}
