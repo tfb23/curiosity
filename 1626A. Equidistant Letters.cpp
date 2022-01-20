9c21d1d087c1c02fd768e60fa83017a7a4479ff2
Toushiful Ferdous Badhan <68116912+tfbadhan@users.noreply.github.com>
2022-01-20 22:09:45 +0600

    Create 1626A. Equidistant Letters.cpp

---
+++ 1626A. Equidistant Letters.cpp
@@ -0,0 +1,16 @@
+//    https://codeforces.com/contest/1626/problem/A/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+		while(t--) {
+		string s;
+		cin >> s;
+		sort(s.begin(), s.end());
+		cout << s << endl;
+	}
+}
